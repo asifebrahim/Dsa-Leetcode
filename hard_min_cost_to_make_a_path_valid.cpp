6eb6a384274afe99f98f4973322f491c6c4ced01
asifebrahim <jnesa38@gmail.com>
2025-01-18 18:54:45 +0530

    1368 no leetcode, good problem

--- hard_min_cost_to_make_a_path_valid.cpp
+++ hard_min_cost_to_make_a_path_valid.cpp
@@ -3,9 +3,11 @@ public:
     int minCost(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        priority_queue<pair<int, pair<int, pair<int, int>>>, vector<pair<int, pair<int, pair<int, int>>>>, greater<>> pq;
+        priority_queue<pair<int, pair<int, pair<int, int>>>, vector<pair<int, pair<int, pair<int, int>>>>, greater<>> pq; 
+        //pq to store {count,direction,row,col}
         set<tuple<int, int, int>> st;
-        pq.push({0, {grid[0][0], {0, 0}}});
+        //set to store visited row,col with directions
+        pq.push({0, {grid[0][0], {0, 0}}});//start with the first pos 0,0
         int ans = INT_MAX;
 
         while (!pq.empty()) {
@@ -17,20 +19,22 @@ public:
             int row = it.second.second.first;
             int col = it.second.second.second;
 
-            if (st.count({row, col, dir})) continue;
-            st.insert({row, col, dir});
+            if (st.count({row, col, dir})) continue; //if this pattern already checked skip
+            st.insert({row, col, dir}); //if not insert it
 
             if (row == m - 1 && col == n - 1) {
-                ans = min(ans, count);
+                ans = min(ans, count); //calculate ans
                 continue;
             }
 
+            //check for all other directions except that dir present as there could be a optimal was like if its -> then check for <- and |^ and down also
             for (int i = 1; i <= 4; i++) {
                 if (i != dir) {
                     pq.push({count + 1, {i, {row, col}}});
                 }
             }
 
+            // goto the directed index accorfin to the grid dir value currently we are on 
             if (dir == 1 && col + 1 < n) {
                 pq.push({count, {grid[row][col + 1], {row, col + 1}}});
             } else if (dir == 2 && col - 1 >= 0) {
