ea18ac1f38665fde180cdb552409332e2cf7035a
asifebrahim <jnesa38@gmail.com>
2025-01-23 00:02:42 +0530

    Added how to remove th leaves

--- Leaves_elimination_in_undirected_graph.cpp
+++ Leaves_elimination_in_undirected_graph.cpp
@@ -6,27 +6,27 @@ public:
         vector<vector<int>> adjList(n);
         vector<int> inDegree(n,0);
         for(auto it:edges){
-            adjList[it[0]].push_back(it[1]);
+            adjList[it[0]].push_back(it[1]);   // Creating the adjacency List
             adjList[it[1]].push_back(it[0]);
-            inDegree[it[0]]++;
+            inDegree[it[0]]++;   // Creating the degree List of each node
             inDegree[it[1]]++;
         }
-        queue<int> q;
+        queue<int> q;               //Creating a queue to push every node with a degree of 1
         for(int i=0;i<n;i++){
             if(inDegree[i]==1){
                 q.push(i);
             }
         }
-        int remain=n;
-        while(remain>2){
-            int leavesCount=q.size();
-            remain-=leavesCount;
-            for(int i=0;i<leavesCount;i++){
+        int remain=n;           //haven't remoeved the leaves yet so remaining is n
+        while(remain>2){          //Ususally a grapg has 2 or 1 MHT
+            int leavesCount=q.size();   //Total node with inDegree 1
+            remain-=leavesCount;    //After removing the leaves we gonna have this much remaining nodes
+            for(int i=0;i<leavesCount;i++){            // For the leaves size we do operations on each of them
                 int x=q.front();
-                q.pop();
+                q.pop();                      //Pop the leaf node
                 for(int it:adjList[x]){
-                    inDegree[it]--;
-                    if(inDegree[it]==1){
+                    inDegree[it]--;       //Since we are removing the leaf node we decrease the degree of the nodes connected to it
+                    if(inDegree[it]==1){  //If the degree of the node becomes 1 we push it to the queue as it becomes a leaf node
                         q.push(it);
                     }
                 }
@@ -35,7 +35,7 @@ public:
         }
         vector<int> ans;
         while(!q.empty()){
-            ans.push_back(q.front());
+            ans.push_back(q.front()); //We add the remaining nodes to the answer vector
             q.pop();
         }
         return ans;
